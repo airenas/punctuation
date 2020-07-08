2c2fc493d55329b3a63b0accfb9e80b289336c36
airenas <airenass@gmail.com>
2020-07-08 15:07:57 +0300

    Drop deprecated gru initialization

--- ptf/model/model.py
+++ ptf/model/model.py
@@ -18,17 +18,13 @@ def init(vocabularySize=1, punctuationSize=1, timesteps=50, word_vector_size=100
         word_vec = layers.Dense(word_vector_size, input_dim=vocabularySize, name='feat_matrix', use_bias=True,
                                 activation=tf.nn.tanh)(word_ids)
     # encoder layer
-    gru_input, f_state, b_state = layers.Bidirectional(
-        __createGRULayer(hidden, gpu, True, 'gru_input'))(word_vec)
+    gru_input, f_state, b_state = layers.Bidirectional(__createGRULayer(hidden, True, 'gru_input'))(word_vec)
 
     # decoder
-    gru_output = __createGRULayer(
-        hidden, gpu, False, 'gru')([gru_input, b_state])
+    gru_output = __createGRULayer(hidden, False, 'gru')([gru_input, b_state])
 
-    attention_output = la.Attention(name='attention_layer')(
-        [gru_input, gru_output, b_state])
-    lf_output = llf.LateFusion(name='late_fusion')(
-        [gru_output, attention_output])
+    attention_output = la.Attention(name='attention_layer')([gru_input, gru_output, b_state])
+    lf_output = llf.LateFusion(name='late_fusion')([gru_output, attention_output])
 
     # output
     out = layers.Dense(punctuationSize, name='out')(lf_output)
@@ -47,7 +43,5 @@ def load(file):
                                                          'Attention': la.Attention})
 
 
-def __createGRULayer(hidden, gpu, returnState, lName):
-    if gpu:
-        return tf.compat.v1.keras.layers.CuDNNGRU(hidden, return_sequences=True, return_state=returnState, name=lName)
+def __createGRULayer(hidden, returnState, lName):
     return layers.GRU(hidden, return_sequences=True, return_state=returnState, name=lName)
