a3a1c942529bde2e547b0328e9717f6e275f1d03
Airenas Vaiciunas <airenass@gmail.com>
2023-12-06 14:01:05 +0200

    Refactor predict to use less RAM

--- ptf/predict.py
+++ ptf/predict.py
@@ -1,16 +1,19 @@
 # coding: utf-8
-import model.model as model
-import data.data as data
-from tensorflow import keras
-import numpy as np
 import sys
-import io 
+
+import numpy as np
+from tqdm import tqdm
+
+import data.data as data
+import model.model as model
 
 MAX_SUBSEQUENCE_LEN = 50
+
 
 def to_array(arr, dtype=np.int32):
     # minibatch of 1 sequence as column
     return np.array([arr], dtype=dtype).T
+
 
 def convert_punctuation_to_readable(punct_token):
     if punct_token == data.SPACE:
@@ -18,31 +21,33 @@ def convert_punctuation_to_readable(punct_token):
     else:
         return punct_token[0]
 
-def progress(count, total, suffix=''):
-    bar_len = 60
-    filled_len = int(round(bar_len * count / float(total)))
 
-    percents = round(100.0 * count / float(total), 1)
-    bar = '=' * filled_len + '-' * (bar_len - filled_len)
-
-    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', suffix))
-    sys.stdout.flush()  # As suggested by Rom Ruben        
-
-def restore(output_file, text, word_vocabulary, reverse_punctuation_vocabulary, predict_function):
-    l = len(text)
+def restore(output_file, text_iter, word_vocabulary, reverse_punctuation_vocabulary, predict_function):
     info = "Punctuating"
+
+    progress_bar = tqdm(desc="Processing", unit=" words")
+
+    def fill_text(text):
+        for item in text_iter:
+            text.append(item)
+            progress_bar.update(1)
+            if len(text) >= MAX_SUBSEQUENCE_LEN:
+                break
+        return text
+
     i = 0
-    progress(i, l, info)
+    text = []
     with open(output_file, 'w', encoding='utf-8') as f_out:
         while True:
-            subsequence = text[i:i+MAX_SUBSEQUENCE_LEN]
+            text = fill_text(text[i:])
+            subsequence = text
             if len(subsequence) == 0:
                 break
             if len(subsequence) < MAX_SUBSEQUENCE_LEN:
-                subsequence = subsequence + ([data.END] * (MAX_SUBSEQUENCE_LEN - len(subsequence)))     
+                subsequence = subsequence + ([data.END] * (MAX_SUBSEQUENCE_LEN - len(subsequence)))
 
             converted_subsequence = [word_vocabulary.get(w, word_vocabulary[data.UNK]) for w in subsequence]
-            
+
             a = np.array(converted_subsequence).reshape(1, len(converted_subsequence))
 
             y = predict_function(a)
@@ -55,11 +60,11 @@ def restore(output_file, text, word_vocabulary, reverse_punctuation_vocabulary, 
 
                 p_i = np.argmax(y_t.flatten())
                 punctuation = reverse_punctuation_vocabulary[p_i]
-                
+
                 punctuations.append(punctuation)
 
                 if punctuation in data.EOS_TOKENS:
-                    last_eos_idx = len(punctuations) # we intentionally want the index of next element
+                    last_eos_idx = len(punctuations)  # we intentionally want the index of next element
 
             if subsequence[-1] == data.END:
                 step = len(subsequence) - 1
@@ -71,15 +76,27 @@ def restore(output_file, text, word_vocabulary, reverse_punctuation_vocabulary, 
             for j in range(step):
                 f_out.write(" " + punctuations[j] + " " if punctuations[j] != data.SPACE else " ")
                 if j < step - 1:
-                    if subsequence[1+j] == data.END:
-                        break    
-                    f_out.write(subsequence[1+j])
+                    if subsequence[1 + j] == data.END:
+                        break
+                    f_out.write(subsequence[1 + j])
 
             if subsequence[-1] == data.END:
                 break
 
-            i += step
-            progress(i, l, info)
+            i = step
+
+        progress_bar.close()
+
+
+def word_iterator(file_path, punctuation_vocabulary):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            words = line.split()
+            for w in words:
+                if w not in punctuation_vocabulary:
+                    yield w
+    yield data.END
+
 
 ############################################################################
 if len(sys.argv) > 1:
@@ -111,18 +128,11 @@ m.summary(150)
 
 punctuation_vocabulary = data.toDict(data.PUNCTUATION_VOCABULARY)
 
-reverse_word_vocabulary = {v:k for k,v in vocab.items()}
-reverse_punctuation_vocabulary = {v:k for k,v in punctuation_vocabulary.items()}
+reverse_word_vocabulary = {v: k for k, v in vocab.items()}
+reverse_punctuation_vocabulary = {v: k for k, v in punctuation_vocabulary.items()}
 
-input_text = io.open(in_file, 'r', encoding='utf-8').read()
+predict = lambda x: m.predict(x, verbose=0)
 
-if len(input_text) == 0:
-    sys.exit("Input text missing.")
-
-text = [w for w in input_text.split() if w not in punctuation_vocabulary] + [data.END]
-
-predict = lambda x : m.predict(x, verbose=0)
-
-restore(output_file, text, vocab, reverse_punctuation_vocabulary, predict)
+restore(output_file, word_iterator(in_file, punctuation_vocabulary), vocab, reverse_punctuation_vocabulary, predict)
 
 print("Done")
